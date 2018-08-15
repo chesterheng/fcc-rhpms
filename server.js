3fb53dd5de157e47cd7b2b78f80aa4b0eec7bfed
Glitch (fcc-rhpms) <none>
2018-08-15 22:56:05 +0000

    😄🚒 Checkpoint ./server.js:1543781/488

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    "ipaddress": req.ip,
+    "language": req.get('accept-language'),
+    "software": req.get('user-agent')
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
